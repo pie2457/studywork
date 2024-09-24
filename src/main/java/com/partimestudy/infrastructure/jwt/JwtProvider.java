ad8684a8c9b715121253315896cb9124df5524c6
pie <yejinh_h@naver.com>
2024-09-24 14:35:32 +0900

    feat : jwt 생성 기능 구현

---
+++ src/main/java/com/partimestudy/infrastructure/jwt/JwtProvider.java
@@ -0,0 +1,55 @@
+package com.partimestudy.infrastructure.jwt;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.Map;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.stereotype.Component;
+
+import com.partimestudy.assignment.domain.exception.ErrorCode;
+import com.partimestudy.assignment.domain.exception.UnAuthorizedException;
+import com.partimestudy.infrastructure.common.properties.JwtProperties;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtProvider {
+    private final SecretKey secretKey;
+    private final long accessTokenExpirationTime;
+
+    public JwtProvider(JwtProperties properties) {
+        this.secretKey = Keys.hmacShaKeyFor(properties.secretKey().getBytes(StandardCharsets.UTF_8));
+        this.accessTokenExpirationTime = properties.accessTokenExpirationTime();
+    }
+
+    public String createAccessToken(String userToken) {
+        Date now = new Date();
+        Date accessTokenExpiration = new Date(now.getTime() + accessTokenExpirationTime);
+
+        return Jwts.builder()
+            .signWith(secretKey, SignatureAlgorithm.HS256)
+            .setIssuedAt(now)
+            .setExpiration(accessTokenExpiration)
+            .addClaims(Map.of("userToken", userToken))
+            .compact();
+    }
+
+    public void validateToken(final String token) {
+        try {
+            Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJwt(token);
+        } catch (ExpiredJwtException e) {
+            throw new UnAuthorizedException(ErrorCode.EXPIRED_TOKEN);
+        } catch (JwtException e) {
+            throw new UnAuthorizedException(ErrorCode.INVALID_TOKEN);
+        }
+    }
+}
