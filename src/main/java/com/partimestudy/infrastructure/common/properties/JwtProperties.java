21dba8f970b1e8caf7fd9a0e85c7620cb7ba2c76
pie <yejinh_h@naver.com>
2024-09-24 14:33:46 +0900

    feat : .yml 파일에서 환경변수를 읽어오도록 설정

---
+++ src/main/java/com/partimestudy/infrastructure/common/properties/JwtProperties.java
@@ -0,0 +1,12 @@
+package com.partimestudy.infrastructure.common.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("jwt")
+public record JwtProperties(
+    String secretKey,
+    long accessTokenExpirationTime,
+    long refreshTokenExpirationTime
+) {
+
+}
